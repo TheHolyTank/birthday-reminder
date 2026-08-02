1ed6a4929c8b402e125e665feb949a3e255310de
TheHolyTank <asaf112004@gmail.com>
2026-08-02 13:54:25 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./app/**/*.{js,jsx}"],
+  content: ["./app/**/*.{js,jsx}", "./lib/**/*.{js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
